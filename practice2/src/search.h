76acc50a0ea9f549b46efd57970ba6684b51163f
diduk001 <didurenkos@gmail.com>
2026-05-22 10:44:32 +0300

    Added declarations in search.h

---
+++ practice2/src/search.h
@@ -0,0 +1,17 @@
+/**
+ * @file search.h
+ * @brief Объявление методов поиска спортсменов в массиве
+ */
+
+#pragma once
+
+#include "sportsman.h"
+#include <vector>
+
+std::vector<int> linear_search(const std::vector<Sportsman> &sportsmen, const Sportsman &target);
+
+std::vector<int> binary_tree_search(const std::vector<Sportsman> &sportsmen, const Sportsman &target);
+
+std::vector<int> red_black_tree_search(const std::vector<Sportsman> &sportsmen, const Sportsman &target);
+
+std::vector<int> hash_table_search(const std::vector<Sportsman> &sportsmen, const Sportsman &target);
