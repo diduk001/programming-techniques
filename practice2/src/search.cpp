ede821b0391bba708f1aad0ecddaebc6ca4a7caa
diduk001 <didurenkos@gmail.com>
2026-05-22 13:00:59 +0300

    Added RedBlackTree and RBNode

--- practice2/src/search.cpp
+++ practice2/src/search.cpp
@@ -130,3 +130,241 @@ std::vector<int> binary_tree_search(const std::vector<Sportsman> &sportsmen, con
     bst.search(target, indices);
     return indices;
 }
+
+/*
+ * @enum Color
+ * @brief Цвет узла в красно-черном дереве
+ */
+enum Color
+{
+    RED,  /**< Красный цвет узла */
+    BLACK /**< Черный цвет узла */
+};
+
+/**
+ * @struct RBNode
+ * @brief Узел красно-черного дерева
+ */
+struct RBNode
+{
+    Sportsman value;   /**< Значение узла */
+    int originalIndex; /**< Индекс спортсмена в исходном массиве */
+    Color color;       /**< Цвет узла */
+    RBNode *left;      /**< Указатель на левое поддерево */
+    RBNode *right;     /**< Указатель на правое поддерево */
+    RBNode *parent;    /**< Указатель на родителя */
+
+    /**
+     * @brief Конструктор, который инициализирует узел на основе спортсмена
+     * @param[val] Спортсмен для хранения в узле
+     */
+    RBNode(const Sportsman &val, int idx)
+        : value(val), originalIndex(idx), color(Color::RED), left(nullptr), right(nullptr), parent(nullptr) {}
+};
+
+/**
+ * @class RedBlackTree
+ * @brief Красно-черное дерево
+ */
+struct RedBlackTree
+{
+    RBNode *root; /**< Корень дерева */
+
+    /**
+     * @brief Конструктор, который инициализирует пустое дерево
+     */
+    RedBlackTree() : root(nullptr) {}
+
+    /**
+     * @brief Вставляет спортсмена в красно-черное дерево
+     * @param[value] Спортсмен для вставки
+     */
+    void insert(const Sportsman &value, int index)
+    {
+        RBNode *z = new RBNode(value, index);
+        RBNode *y = nullptr;
+        RBNode *x = root;
+
+        while (x)
+        {
+            y = x;
+            if (value < x->value)
+            {
+                x = x->left;
+            }
+            else
+            {
+                x = x->right;
+            }
+        }
+
+        z->parent = y;
+        if (!y)
+        {
+            root = z;
+        }
+        else if (value < y->value)
+        {
+            y->left = z;
+        }
+        else
+        {
+            y->right = z;
+        }
+
+        fix_insert(z);
+    }
+
+    /**
+     * @brief Поворачивает узел влево
+     * @param[x] Узел для поворота
+     */
+    void rotate_left(RBNode *x)
+    {
+        if (!x || !x->right)
+            return;
+        RBNode *y = x->right;
+        x->right = y->left;
+        if (x->right)
+            x->right->parent = x;
+        y->parent = x->parent;
+
+        if (!x->parent)
+        {
+            root = y;
+        }
+        else if (x == x->parent->left)
+        {
+            x->parent->left = y;
+        }
+        else
+        {
+            x->parent->right = y;
+        }
+        y->left = x;
+        x->parent = y;
+    }
+
+    /**
+     * @brief Поворачивает узел вправо
+     * @param[y] Узел для поворота
+     */
+    void rotate_right(RBNode *y)
+    {
+        if (!y || !y->left)
+            return;
+        RBNode *x = y->left;
+        y->left = x->right;
+        if (y->left)
+            y->left->parent = y;
+        x->parent = y->parent;
+
+        if (!y->parent)
+        {
+            root = x;
+        }
+        else if (y == y->parent->left)
+        {
+            y->parent->left = x;
+        }
+        else
+        {
+            y->parent->right = x;
+        }
+        x->right = y;
+        y->parent = x;
+    }
+
+    /**
+     * @brief Исправляет дерево после вставки узла
+     * @param[z] Вставленный узел, который может нарушать свойства красно-черного дерева
+     */
+    void fix_insert(RBNode *z)
+    {
+        while (z != root && z->parent->color == Color::RED)
+        {
+            if (z->parent == z->parent->parent->left)
+            {
+                RBNode *y = z->parent->parent->right;
+                if (y && y->color == Color::RED)
+                {
+                    z->parent->color = Color::BLACK;
+                    y->color = Color::BLACK;
+                    z->parent->parent->color = Color::RED;
+                    z = z->parent->parent;
+                }
+                else
+                {
+                    if (z == z->parent->right)
+                    {
+                        z = z->parent;
+                        rotate_left(z);
+                    }
+                    z->parent->color = Color::BLACK;
+                    z->parent->parent->color = Color::RED;
+                    rotate_right(z->parent->parent);
+                }
+            }
+            else
+            {
+                RBNode *y = z->parent->parent->left;
+                if (y && y->color == Color::RED)
+                {
+                    z->parent->color = Color::BLACK;
+                    y->color = Color::BLACK;
+                    z->parent->parent->color = Color::RED;
+                    z = z->parent->parent;
+                }
+                else
+                {
+                    if (z == z->parent->left)
+                    {
+                        z = z->parent;
+                        rotate_right(z);
+                    }
+                    z->parent->color = Color::BLACK;
+                    z->parent->parent->color = Color::RED;
+                    rotate_left(z->parent->parent);
+                }
+            }
+        }
+        root->color = Color::BLACK;
+    }
+
+    /**
+     * @brief Ищет спортсмена в поддереве, начиная с данного узла
+     * @param[target] Спортсмен для поиска
+     * @param[result] Вектор индексов найденных спортсменов, равных target
+     */
+    void search_all(RBNode *node, const Sportsman &target, std::vector<int> &result) const
+    {
+        if (!node)
+            return;
+
+        if (node->value == target)
+        {
+            result.push_back(node->originalIndex);
+            search_all(node->right, target, result);
+        }
+        else if (target < node->value)
+        {
+            search_all(node->left, target, result);
+        }
+        else
+        {
+            search_all(node->right, target, result);
+        }
+    }
+};
+
+std::vector<int> red_black_tree_search(const std::vector<Sportsman> &sportsmen, const Sportsman &target)
+{
+    RedBlackTree rbt;
+    for (int i = 0; i < (int)sportsmen.size(); ++i)
+    {
+        rbt.insert(sportsmen[i], i);
+    }
+    std::vector<int> indices;
+    rbt.search_all(rbt.root, target, indices);
+    return indices;
+}
